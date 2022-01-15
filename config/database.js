f4e57558851230463f912e003ddfe57369d1c04e
Pawedla <pedamain@gmx.at>
2022-01-15 16:51:14 +0100

    changed database

--- config/database.js
+++ config/database.js
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize";
 
  
-const db = new Sequelize('d3oscl2chjcn92', 'vpkpxwnhmhottt', '63bface740f1c18598e8340e61d8aeb09df0945a8525df08ab4e7aab954e7ea0', {
-    host: "ec2-23-23-133-10.compute-1.amazonaws.com",
+const db = new Sequelize('damu6qlav34ufh', 'eqxexbcwuesifh', '8a6d434ab72b3ea6a2c06dfacc7ca01a61861a52e6bf32d1f0f0fa1ab0526ee1', {
+    host: "ec2-54-220-223-3.eu-west-1.compute.amazonaws.com",
     dialect: "postgres",
     dialectOptions: {
         ssl: {
           require: true,
-          rejectUnauthorized: false // <<<<<<< YOU NEED THIS
+          rejectUnauthorized: false
         }
       },
     port: 5432,
